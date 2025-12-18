0306c1e0d3047254b2c0cd37bac48f137edf4a73
SamirCoder16 <samirparvez48@gmail.com>
2025-12-18 19:52:58 +0530

    server js modified

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,12 @@
 import "dotenv/config";
 import express from "express";
-import cors from "cors";
+// import cors from "cors";
 import { ENV } from "./lib/env.js";
-import connectDB from "./config/db_config.js";
-import logger from "./utils/logger.js";
-// import { clerkMiddleware } from "@clerk/express";
-import { serve } from "inngest/express";
-import { inngest, functions } from "./lib/inngest.js";
+// import connectDB from "./config/db_config.js";
+// import logger from "./utils/logger.js";
+// // import { clerkMiddleware } from "@clerk/express";
+// import { serve } from "inngest/express";
+// import { inngest, functions } from "./lib/inngest.js";
 
 const app = express();
 
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use(cors({ origin: "*", credentials: true }));
 
-// // ✅ DB connect per request (serverless-safe)
+// // DB connect per request (serverless-safe)
 // app.use(async (req, res, next) => {
 //   try {
 //     await connectDB();
@@ -29,10 +29,10 @@ app.get("/health", (req, res) => {
   res.send("Server is healthy!");
 });
 
-// // ✅ Inngest
+// // Inngest
 // app.use("/api/inngest", serve({ client: inngest, functions }));
 
-// // ✅ Local development only
+// // Local development only
 // if (ENV.NODE_ENV === "development") {
 //   const PORT = ENV.PORT;
 //   app.listen(PORT, () => {
