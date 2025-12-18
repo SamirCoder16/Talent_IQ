f3f9e07df34f89fae5f50068f31780a66255750c
SamirCoder16 <samirparvez48@gmail.com>
2025-12-18 19:57:45 +0530

    inngest updated

--- server/src/server.js
+++ server/src/server.js
@@ -5,8 +5,8 @@ import { ENV } from "./lib/env.js";
 // import connectDB from "./config/db_config.js";
 // import logger from "./utils/logger.js";
 // // import { clerkMiddleware } from "@clerk/express";
-// import { serve } from "inngest/express";
-// import { inngest, functions } from "./lib/inngest.js";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./lib/inngest.js";
 
 const app = express();
 
@@ -25,12 +25,16 @@ app.use(cors({ origin: "*", credentials: true }));
 // });
 // // app.use(clerkMiddleware());
 
+app.get("/", (req,res) => {
+  res.send("Welcome to the server!");
+});
+
 app.get("/health", (req, res) => {
   res.send("Server is healthy!");
 });
 
 // // Inngest
-// app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 // // Local development only
 // if (ENV.NODE_ENV === "development") {
