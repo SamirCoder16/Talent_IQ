5ee0f4bafea5b9aa0a3df3b2194de0cdeca6b183
SamirCoder16 <samirparvez48@gmail.com>
2025-12-20 19:01:57 +0530

    stream sync added

---
+++ server/src/config/stream_config.js
@@ -0,0 +1,38 @@
+import { StreamChat } from "stream-chat";
+import { ENV } from "../lib/env.js";
+import logger from "../utils/logger.js";
+
+const apikey = ENV.STREAM_API_KEY;
+const apisecret = ENV.STREAM_API_SECRET;
+
+if(!apikey || !apisecret) {
+    throw new Error("Stream API key and secret must be set in enviroment variables.");
+}
+
+export const chatClient = StreamChat.getInstance(apikey, apisecret);
+
+export const upsertStreamUser = async (userData) => {
+    try {
+        await chatClient.upsertUsers([userData]);
+        return userData;
+    } catch (error) {
+        logger.info("Error upserting Stream user:", error);
+        throw error;
+    }
+}
+
+export const deleteStreamUser = async (userId) => {
+    try {
+        await chatClient.deleteUser(userId, { markMessagesDeleted: true });
+        try {
+            logger.info(`Stream user with ID ${userId} deleted successfully.`);
+        } catch (error) {
+            console.log(`Stream user with ID ${userId} deleted successfully.`);
+        }
+    } catch (error) {
+        logger.error("Error deleting Stream user:", error);
+        throw error;
+    }
+}
+
+// TODO: to generate Token ;
